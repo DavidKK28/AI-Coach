19fb95ceda9ba0579d8012aafe5e32108dc3ed61
DavidKK28 <127589236+DavidKK28@users.noreply.github.com>
2023-03-20 22:17:56 +0900

    Revert "Update line.js"
    This reverts commit 5144f359dec93962379bb40dc40ed5c08a3188d8.

--- line.js
+++ line.js
@@ -1,69 +1,36 @@
-// line.js
-const azureTTS = require("./azure-tts");
-const azureSpeech = require("./azure-speech");
-const fs = require("fs");
-const path = require("path");
-const line = require("@line/bot-sdk");
+// Set line bot config 
+const azureTTS = require('./azure-tts');
+const fs = require('fs');
+const path = require('path');
+const line = require('@line/bot-sdk')
 const config = {
   channelAccessToken: process.env.channelAccessToken,
   channelSecret: process.env.channelSecret
-};
-const client = new line.Client(config);
-const openai = require("./openai");
-const middleware = line.middleware(config);
+}
+const client = new line.Client(config)
+const openai = require('./openai')
+const middleware = line.middleware(config)
 
 // Handles an incoming LINE Messaging API event
-const handleEvent = async (event) => {
-  if (event.type === "message") {
-    if (event.message.type === "text") {
-      const textResponse = await openai.chatGPT(event.message.text);
-      const audioFilePath = await azureTTS.textToSpeech(textResponse);
-
-      const audioMessage = {
-        type: "audio",
-        originalContentUrl: audioFilePath,
-        duration: 60000
-      };
-
-      return client.replyMessage(event.replyToken, audioMessage);
-    } else if (event.message.type === "audio") {
-      // Download audio file from LINE server
-      const audioStream = await client.getMessageContent(event.message.id);
-      const audioFilePath = path.join(__dirname, "tempAudio.wav");
-      const writeStream = fs.createWriteStream(audioFilePath);
-      audioStream.pipe(writeStream);
-
-      writeStream.on("finish", async () => {
-        try {
-          const recognizedText = await azureSpeech.speechToText(audioFilePath);
-          fs.unlinkSync(audioFilePath); // Remove temporary audio file
-      
-          const textResponse = await openai.chatGPT(recognizedText);
-          const audioResponsePath = await azureTTS.textToSpeech(textResponse);
-      
-          const audioMessage = {
-            type: "audio",
-            originalContentUrl: audioResponsePath,
-            duration: 60000
-          };
-      
-          return client.replyMessage(event.replyToken, audioMessage);
-        } catch (error) {
-          console.error("Error processing audio:", error);
-        }
-      });
-      
-      writeStream.on("error", (error) => {
-        console.error("Error writing audio file:", error);
-      });
-      
-    }
+const handleEvent = async (event) =>{
+  if (event.type === 'message' && event.message.type === 'text') {
+    const textResponse = await openai.chatGPT(event.message.text);
+    const audioFilePath = await azureTTS.textToSpeech(textResponse);
+  
+    const audioMessage = {
+      type: 'audio',
+      originalContentUrl: audioFilePath,
+      duration: 60000
+    };
+  
+    return client.replyMessage(event.replyToken, audioMessage);
   }
+  
 
-  return Promise.resolve(null);
-};
+  return Promise.resolve(null)
+}
 
 module.exports = {
   middleware,
   handleEvent
-};
+}
